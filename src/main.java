b5ff863bb7dd43e777ddd8e82f6c7deccf553ab0
Morpheus239 <user@mail.de>
2012-11-06 12:34:30 +0100

    Version 1.0 neue Methode

--- src/main.java
+++ src/main.java
@@ -7,9 +7,13 @@ public class main {
 	public static void main(String[] args) {
 	// TODO Auto-generated method stub
 		System.out.println("This is a test Projekt");
+		ausgabe();
 	}
 
 	
+	private static void ausgabe() {
+		System.out.println("Version 1.0");
+	}
 	
 	
 	
